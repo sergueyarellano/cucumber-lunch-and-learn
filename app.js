6353b89cc9bd947e44dccb9b076341cb686d0617
Serguey Martinez <sarellano85@gmail.com>
2019-11-27 09:52:01 -0500

    feat: add simple level db example

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const level = require('level')
+const db = level('my-db')
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(morgan('common'))
-app.all('/', (req, res, next) => res.send('Hello World!'))
+
+app.use(function (req, res, next) {
+  db.put('name', 'world', function (err) {
+    if (err) return console.log('Ooops!', err) // some kind of I/O error
+    next()
+  })
+})
+app.all('/', (req, res, next) => {
+  // Fetch by key
+  db.get('name', function (err, value) {
+    if (err) return console.log('Ooops!', err) // likely the key was not found
+
+    res.send(`Hello ${value}!`)
+  })
+})
 
 app.listen(port, () => console.log(`Magic happens on port ${port}!`))
